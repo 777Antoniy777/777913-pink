31d2a09a462046fb5c36210c3cc58be070ba1e5f
Антон <tat7106@gmail.com>
2018-10-29 10:53:07 +0600

    проблемы с автоматизацией 2

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,9 @@ var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var css_min = require("gulp-csso");
+var rename = require("gulp-rename");
+var image_min = require("gulp-imagemin");
 var server = require("browser-sync").create();
 
 gulp.task("css", function () {
@@ -15,7 +18,18 @@ gulp.task("css", function () {
       autoprefixer()
     ]))
     .pipe(gulp.dest("source/css"))
+    .pipe(css_min())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
+});
+
+gulp.task("images", function() {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(image_min([
+      image_min.optipng({optimizationLevel: 3})
+    ]))
+    .pipe(gulp.dest("source/img"));
 });
 
 gulp.task("server", function () {
